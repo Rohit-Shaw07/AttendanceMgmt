079d0cf2250cdb9e89108fcd912a29f70c4861fe
Rohit <Rohit@user>
2021-05-10 10:17:51 +0530

    Rohit commited it.

---
+++ AttendenceMgmt/src/main/java/com/cg/AttendenceMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendenceMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendenceMgmtApplication.class, args);
+	}
+
+}
